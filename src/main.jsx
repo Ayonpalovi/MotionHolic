7702129029d4c315bf6cbb9fdf05d3787a93c4bd
Ayonpalovi <ovip2523@gmail.com>
2026-05-14 00:42:14 +0200

    Use MotionHolic OS process workflow

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import PrivacyNotice from './components/PrivacyNotice.jsx'
 import MZTestimonialCarousel from './components/MZTestimonialCarousel.jsx'
 import BlendedStatsSection from './components/BlendedStatsSection.jsx'
+import OSProcessSection from './components/OSProcessSection.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <BlendedStatsSection />
+    <OSProcessSection />
     <MZTestimonialCarousel />
     <PrivacyNotice />
   </React.StrictMode>,
