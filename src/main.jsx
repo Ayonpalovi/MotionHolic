170b9fc31c0a1509e98a794af2acc5b9a507cf85
Ayonpalovi <ovip2523@gmail.com>
2026-05-14 00:11:22 +0200

    Use blended background stats section

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import PrivacyNotice from './components/PrivacyNotice.jsx'
 import MZTestimonialCarousel from './components/MZTestimonialCarousel.jsx'
+import BlendedStatsSection from './components/BlendedStatsSection.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <BlendedStatsSection />
     <MZTestimonialCarousel />
     <PrivacyNotice />
   </React.StrictMode>,
