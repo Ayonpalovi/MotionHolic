9d14b9c528f008b392a0b79136c0d3ec28a9fe54
Ayonpalovi <ovip2523@gmail.com>
2026-05-14 14:11:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -728,7 +728,7 @@ export default function MotionHolicPortfolio() {
           </div>
         </section>
 
-        <section id="process" ref={processRef} className="relative px-4 py-28 sm:px-6 lg:px-8">
+        <section id="process" ref={processRef} className="relative px-4 py-16 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-7xl">
             <div className="mb-16 text-center">
               <SectionLabel>Our process</SectionLabel>
